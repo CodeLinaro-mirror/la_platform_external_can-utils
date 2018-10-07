de6b200b2e142992e634f85fde3afa1101748706
sumesahu <sumesahu@codeaurora.org>
2018-10-07 21:36:16 +0530

    Modify android.mk to supress warnings as errors for Android P build
    Change-Id: I1f6ae29daaf19b73bc707b6c6cda31efe8f8afa6

--- Android.mk
+++ Android.mk
@@ -3,8 +3,8 @@ LOCAL_PATH := $(call my-dir)
 PRIVATE_LOCAL_CFLAGS := -O2 -g -W -Wall		\
 			-DSO_RXQ_OVFL=40	\
 			-DPF_CAN=29		\
-			-DAF_CAN=PF_CAN
-
+			-DAF_CAN=PF_CAN \
+			-Wno-sign-compare -Wno-unused-parameter -Wno-pointer-arith -Wno-address-of-packed-member -Wno-absolute-value\
 #
 # canlib
 #
